b411ab1bb1cd9d51f6792c4f939d2c38f38e1965
Deniz95140 <120654384+Deniz95140@users.noreply.github.com>
2023-10-05 19:08:04 +0200

    Update gestionnaire_de_tache.py

--- gestionnaire_de_tache.py
+++ gestionnaire_de_tache.py
@@ -4,45 +4,58 @@ task = {}
 
 print("Bienvenue dans votre gestionnaire de tâches !")
 
+def afficher_tache():
+    if not task:
+        print("Il n'y a aucune tâche.")
+    else:
+        for keys, values in task.items():
+            print(f"{keys} {values}\n")
+
+def ajouter_tache():
+    print("Ajouter une tâche: ")
+    add_task_key = input()
+    task[add_task_key] = "[ ]"
+    print("Tâche ajouté !")
+
+def supprimer_tache():
+    print("Quelle tâche souhaitez vous supprimer ? : ")
+    delete_task_key = input()
+    if delete_task_key in task.keys():
+        del task[delete_task_key]
+        print("Tâche supprimé !")
+    else:
+        print("Erreur, cette tâche n'existe pas.")
+
+def definir_tache():
+    print("Quelle tâche souhaitez-vous marqué comme accomplie ? : ")
+    finish_task_key = input()
+    if finish_task_key in task.keys():
+        task[finish_task_key] = "[X]"
+        print("Tâche défini comme accomplit !")
+    else:
+        print("Erreur, cette tâche n'existe pas.")
+
 def gdt():
     while True:
         try:
-            choose = int(input("Que souhaitez-vous faire ?\n 1. Afficher les tâches\n 2. Ajouter une tâche\n 3. Supprimer une tâche\n 4. Définir une tâche comme accomplie\n 5. Quitter le gestionnaire de tâches"))
-            if choose not in [1, 2, 3, 4]:
-                raise ValueError("Erreur")
-
+            choose = int(input("\nQue souhaitez-vous faire ?\n\n 1. Afficher les tâches\n 2. Ajouter une tâche\n 3. Supprimer une tâche\n 4. Définir une tâche comme accomplie\n 5. Quitter le gestionnaire de tâches\n"))
             if choose == 1:
-                for keys, values in task.items():
-                    print(f"{keys} {values}\n")
+                afficher_tache()
 
-            elif choose == 2:
-                print("Ajouter une tâche:")
-                add_task_key = input()
-                task[add_task_key] = "[ ]"
-                print("Tâche ajouté !")
+            if choose == 2:
+                ajouter_tache()
 
-            elif choose == 3:
-                print("Quelle tâche souhaitez-vous supprimer ?")
-                delete_task_key = input()
-                if delete_task_key in task:
-                    del task[delete_task_key]
-                    print("tâche supprimé!")
-                else:
-                    print("Erreur: cette tâche n'existe pas.")
+            if choose == 3:
+                supprimer_tache()
 
-            elif choose == 4:
-                print("Quelle tâche souhaitez-vous marqué comme accomplie ?")
-                finish_task_key = input()
-                if finish_task_key in task.keys():
-                    task[finish_task_key] = "[X]"
-                    print("Tâche ajouté comme accomplie")
-                else:
-                    print("Erreur: cette tâche n'existe pas.")
-
+            if choose == 4:
+                definir_tache()
             elif choose == 5:
                 print("Au revoir!")
                 sys.exit()
 
         except ValueError:
             print("Erreur, choissiez un nombre entre 1, 2, 3, 4 et 5.")
-gdt()
+
+if __name__ == '__main__':
+    gdt()
